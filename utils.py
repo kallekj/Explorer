b3beb0d7bd7e73e31efa9c7466584eb07aa40caa
AndreasH96 <andreashaggstrom96@gmail.com>
2021-01-13 12:39:48 +0100

    Added UK dataset parser

---
+++ utils.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+def parse_UK_Data(fileName):
+    
+    
+    data = pd.read_csv(fileName, header = None,sep='\n',engine="python")
+    metaData = data[:3]
+    data = data[3:]
+    
+    #==========PARSE METADATA===============
+    metaData = metaData.T
+    # Extract data to numpy arrays
+    curbWeight_maxLoad = metaData[1].str.split('\t').to_numpy()[0]
+    minMaxSpeed = metaData[2].str.split('\t').to_numpy()[0]
+    #Create columns to hold the data
+    metaData["Customer Amount"] = metaData[0]
+    metaData["Vehicle Curb Weight(kg)"] = curbWeight_maxLoad[0]
+    metaData["Max Load(kg)"] = curbWeight_maxLoad[1]
+    metaData["Minimum Speed(km/h)"] = minMaxSpeed[0]
+    metaData["Maximum Speed(km/h)"] = minMaxSpeed[1]
+    #Drop the original columns
+    metaData.drop([0,1,2],axis=1,inplace=True)
+
+    
+    
+    #==========PARSE DISTANCE MATRIX ============
+    distance_Data = data[:int(len(data)/2)]
+    distance_Data.reset_index(inplace = True,drop=True) 
+    distance_Data = distance_Data.T
+    
+    for index,_ in enumerate(distance_Data):
+         distance_Data[index] = distance_Data[index].str.split('\t')
+
+    distance_Data = distance_Data.T[0].apply(pd.Series)
+
+    #Drop last column since data set has dangling tab
+    distance_Data.drop([int(len(data)/2)],axis=1,inplace=True)
+
+
+    #=========PARSE CITY DATA ===================
+    station_Data = data[int(len(data)/2):]
+    station_Data.reset_index(inplace = True,drop=True) 
+    station_Data = station_Data.T
+    
+    for index,_ in enumerate(station_Data):
+        station_Data[index] = station_Data[index].str.split('\t|\s{3,}')
+        
+    station_Data = station_Data.T[0].apply(pd.Series)
+    station_Data.drop([0,3,5,7],axis=1,inplace=True)
+    station_Data.columns = ["City Name", "Demand(kg)","Ready Time(sec)","Due Time(sec)","Service Time(sec)"]
+
+    
+    return metaData, distance_Data, station_Data
